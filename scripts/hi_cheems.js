1194c1b99b0b3a5951979d19ad53b4bd06f8ddae
Bruce.Wang <bruce.wang@acronis.com>
2022-07-21 02:32:02 -0700

    create hi_cheems

---
+++ scripts/hi_cheems.js
@@ -0,0 +1,13 @@
+const myHeading = document.querySelector('h2');
+myHeading.textContent = 'Cheems!';
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === '/images/doge.jfif') {
+      myImage.setAttribute('src','/images/cheems.jfif');
+    } else {
+      myImage.setAttribute('src','/images/doge.jfif');
+    }
+}
