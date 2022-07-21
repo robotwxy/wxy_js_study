0aa7d5644663697332fdde85aec3540a1ec560e3
Bruce.Wang <bruce.wang@acronis.com>
2022-07-21 02:50:53 -0700

    change text when clicking

--- hi_cheems/scripts/hi_cheems.js
+++ hi_cheems/scripts/hi_cheems.js
@@ -7,7 +7,7 @@ myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/doge.jfif') {
       myImage.setAttribute('src','images/cheems.jfif');
-      myHeading.textContent = "Doge ⌇ຶ_⌇ຶ";
+      myHeading.textContent = "Doge T_T";
     } else {
       myImage.setAttribute('src','images/doge.jfif');
       myHeading.textContent = 'Cheems!';
